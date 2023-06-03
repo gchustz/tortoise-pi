9412133db3c08fff1c11f4d513ecefe87ad2d079
George Chustz <george.chustz@gmail.com>
2023-06-03 09:32:22 -0500

    fixed execution privileges and  typo

--- setup.bash
+++ setup.bash
@@ -8,7 +8,7 @@ cp tortoise_service.py /usr/bin/
 SYSTEMD_PATH=/usr/lib/systemd/system
 cp tortoise_setup.service $SYSTEMD_PATH
 cp tortoise_gpio.service $SYSTEMD_PATH
-cp tortoise_cleanup.service $SYSTEMD_PATH
+cp tortoise_gpio_cleanup.service $SYSTEMD_PATH
 
 sudo systemctl daemon-reload
 sudo systemctl enable tortoise_setup.service
